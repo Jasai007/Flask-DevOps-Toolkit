4684c35161838a6f9bb9a8d09bc67591296368b5
RaptorCodes <jasaihansda24@gmail.com>
2025-04-02 14:24:56 +0530

    Fix formatting in run_jenkins.sh by removing unnecessary line breaks

--- Pipelines/run_jenkins.sh
+++ Pipelines/run_jenkins.sh
@@ -9,7 +9,7 @@ fi
 
 # Run a Jenkins container in detached mode
 docker run -d --name jenkins \
-  -p 8080:8080 -p 50000:50000 \  # Map host ports to container ports
-  --restart=on-failure \       # Restart policy for the container
-  -v jenkins_home:/var/jenkins_home \  # Volume for persistent Jenkins data
-  jenkins/jenkins:lts             # Use the latest LTS version of Jenkins
+  -p 8080:8080 -p 50000:50000 \
+  --restart=on-failure \
+  -v jenkins_home:/var/jenkins_home \
+  jenkins/jenkins:lts
